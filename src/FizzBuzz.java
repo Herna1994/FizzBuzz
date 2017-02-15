f5792d16406c105759a0c8888505f6c8ffd084ec
Hernán Castañón Álvarez <hernanderes34@gmail.com>
2017-02-15 09:43:32 +0100

    FizzBuzz: Añadir FizzBuzzTest y mejorar FizzBuzz
    Signed-off-by: Hernán Castañón Álvarez <hernanderes34@gmail.com>

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,53 +1,57 @@
+package FizzBuzz;
+
 import java.util.Scanner;
 
 public class FizzBuzz {
 
-	public static void main(String[] args) {		
-            Scanner leer = new Scanner(System.in);
-            System.out.println("Introduce el numero: ");
-	    int numero = leer.nextInt();
-	    int decenas = calcularDecenas(numero);
-	    int unidades = calcularUnidades(numero, decenas);
-	    
-	    if (numero < 0 || numero > 100) {
-	    	System.out.println("Número inválido, ¡que pase un buen día!");
-	    	System.exit(0);
-	    }
-	    
-	    if (numero % 3 == 0 && numero % 5 == 0) {
-	      System.out.println("FizzBuzz");
-	    } else {
-	      if (numero % 3 == 0) {
-	        System.out.println("Fizz");
-	      } else if (numero % 5 == 0) {
-	        System.out.println("Buzz");
-	      } else {
-	        makeFizzBuzz(numero, decenas, unidades);
-	      }
-	    }
-	 }
+	public static void main(String[] args) {
+		Scanner leer = new Scanner(System.in);
+		int numero;
+		System.out.println("Introduce el numero: ");
+		numero = leer.nextInt();
+		int decenas = calcularDecenas(numero);
+		int unidades = calcularUnidades(numero, decenas);
 
-	  public static void makeFizzBuzz(int numero, int decenas, int unidades){
-	    String u[]={"","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"};
-	    String e[]={"Ten","Eleven","Twelve","Thirteen","Fourteen","Fiveteen","Sixteen","Seventeen","Eighteen","Nineteen"};
-	    String d[] = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-	    
-	    if (numero > 10 && numero <20) {
-	    	System.out.println(numero + " se escribe " + e[unidades]);
-	    } else {
-	    System.out.println(numero + " se escribe " + d[decenas] + "" + u[unidades]);
-	    }
-	  }
+	        if (numero < 0 || numero > 100) {
+	    	      System.out.println("Número inválido, ¡que pase un buen día!");
+	    	      System.exit(0);
+	        }
 
-	  public static int calcularDecenas(int numero) {
-	    int decenas = 0;
-	    decenas = numero / 10;
-	    return decenas;
-	  }
+		if (numero % 3 == 0 && numero % 5 == 0) {
+			System.out.println("Fizzbuzz");
+		} else {
+			if (numero % 3 == 0) {
+				System.out.println("Fizz");
+			} else if (numero % 5 == 0) {
+				System.out.println("Buzz");
+			} else {
+				makeFizzBuzz(numero, decenas, unidades);
+			}
+		}
+	}
 
-	  public static int calcularUnidades(int numero, int decenas) {
-	    int unidades = 0;
-	    unidades = numero - (decenas * 10);
-	    return unidades;
-	  }
+	public static void makeFizzBuzz(int numero, int decenas, int unidades) {
+		String u[] = { "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+		String e[] = { "", "eleven", "twelve", "thirteen", "fourteen", "fiveteen", "sixteen", "seventeen", "eighteen",
+				"nineteen" };
+		String d[] = { "", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+		if (numero > 10 && numero < 20) {
+			System.out.println(numero + " escribe " + e[unidades]);
+		} else {
+			System.out.println(numero + " escribe " + d[decenas] + " " + u[unidades]);
+		}
+	}
+
+	public static int calcularDecenas(int numero) {
+		int decenas = 0;
+		decenas = numero / 10;
+		return decenas;
+	}
+
+	public static int calcularUnidades(int numero, int decenas) {
+		int unidades = 0;
+		unidades = numero - (decenas * 10);
+		return unidades;
+	}
 }
